95055f26f6ce8cd8af74717aed3546cecb1c7452
dinhvietcuong1996 <dinhvietcuong1996@yahoo.com.vn>
2025-04-09 16:38:11 +0100

    Updata gMLP

--- llava/model/multimodal_projector/builder.py
+++ llava/model/multimodal_projector/builder.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import re
-
+from torch.nn import functional as F 
 from .pooler_projector import PoolerProjector
 
 
@@ -16,13 +16,58 @@ class IdentityMap(nn.Module):
     def config(self):
         return {"mm_projector_type": "identity"}
 
+class SpatialGatingUnit(nn.Module):
+    def __init__(self, d_ffn, seq_len):
+        super().__init__()
+        self.norm = nn.LayerNorm(d_ffn)
+        self.spatial_proj = nn.Conv1d(seq_len, seq_len, kernel_size=1)
+        nn.init.constant_(self.spatial_proj.bias, 1.0)
+
+    def forward(self, x):
+        u, v = x.chunk(2, dim=-1)
+        v = self.norm(v)
+        v = self.spatial_proj(v)
+        out = u * v
+        return out
+    
+class gMLPBlock(nn.Module):
+    def __init__(self, d_model, d_ffn, seq_len):
+        super().__init__()
+        self.norm = nn.LayerNorm(d_model)
+        self.channel_proj1 = nn.Linear(d_model, d_ffn * 2)
+        self.channel_proj2 = nn.Linear(d_ffn, d_model)
+        self.sgu = SpatialGatingUnit(d_ffn, seq_len)
+
+    def forward(self, x):
+        residual = x
+        x = self.norm(x)
+        x = F.gelu(self.channel_proj1(x))
+        x = self.sgu(x)
+        x = self.channel_proj2(x)
+        out = x + residual
+        return out
+
+class gMLP(nn.Module):
+    def __init__(self, d_model=256, d_ffn=512, seq_len=256, num_layers=1):
+        super().__init__()
+        self.model = nn.Sequential(
+                *[gMLPBlock(d_model, d_ffn, seq_len) for _ in range(num_layers)]
+                )
+    def forward(self, x):
+        return self.model(x)
 
 class SimpleResBlock(nn.Module):
     def __init__(self, channels):
         super().__init__()
         self.pre_norm = nn.LayerNorm(channels)
 
-        self.proj = nn.Sequential(nn.Linear(channels, channels), nn.GELU(), nn.Linear(channels, channels))
+        #self.proj = nn.Sequential(nn.Linear(channels, channels), nn.GELU(), nn.Linear(channels, channels))
+
+        self.proj = nn.Sequential(
+                nn.Linear(channels, channels), 
+                gMLP(channels, channels * 2, channels, 1), 
+                nn.Linear(channels, channels))
+
 
     def forward(self, x):
         x = self.pre_norm(x)
@@ -53,7 +98,8 @@ def build_vision_projector(config, delay_load=False, **kwargs):
         res_depth = int(mlp_gelu_resnet_match.group(2))
         modules = [nn.Linear(config.mm_hidden_size, config.hidden_size)]
         for _ in range(1, mlp_depth):
-            modules.append(nn.GELU())
+            #modules.append(nn.GELU())
+            modules.append(gMLP(config.hidden_size, config.hidden_size * 2, config.hidden_size, 1))
             modules.append(nn.Linear(config.hidden_size, config.hidden_size))
         for _ in range(res_depth):
             modules.append(SimpleResBlock(config.hidden_size))
